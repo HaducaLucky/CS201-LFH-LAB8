e15ac2bf99511400f2e4179de92850f81d8e2d23
HaducaLucky <147017329+HaducaLucky@users.noreply.github.com>
2023-10-12 17:09:51 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const favSingers = ["Justin Bieber", "Shawn Mendes", "Charlie Puth"];
+console.log(favSingers);
+const favNumbers = [16, 6, 9, 8];
+console.log(favNumbers);
+const mixedArr = ["string",["otherarray"],123,true];
+
+console.log(favSingers[0]);
+console.log(favSingers[1]);
+console.log(favSingers[2]);
+console.log(favNumbers[0]);
+console.log(favNumbers[1]);
+console.log(favNumbers[2]);
+console.log(favNumbers[3]);
+console.log(mixedArr[0]);
+console.log(mixedArr[1]);
+console.log(mixedArr[2]);
+console.log(mixedArr[3]);
